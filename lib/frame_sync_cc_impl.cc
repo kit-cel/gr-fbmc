0b2f44d33cf7f7a82df08e715567d8826b00da50
Felix Wunsch <uncnr@student.kit.edu>
2014-08-04 13:11:45 +0200

    frame_sync wasnt actually buggy in that case, just a misconfiguration. adapted the flow graphs to work with the new serializer/parallelizer.

--- lib/frame_sync_cc_impl.cc
+++ lib/frame_sync_cc_impl.cc
@@ -191,15 +191,10 @@ namespace gr {
           samples_consumed = d_L;
           if(abs(corr) > d_threshold) // SOF found
           {
-            // estimate and correct remaining frequency and phase offsets
-            //float rem_f_off = estimate_f_off(corr);
-            //float rem_phi_off = estimate_phi_off(in+d_L*d_overlap); 
-            //d_f_off += rem_f_off;
-            //d_phi_off = fmod(d_phi_off + rem_phi_off, 2*M_PI);
+            // estimate and correct frequency and phase offsets
             d_f_off = estimate_f_off(corr);
             d_phi_off = fmod(estimate_phi_off(in+d_L*d_overlap) - 2*M_PI*d_f_off*d_L*d_overlap, 2*M_PI); // the estimated phase offset has to be turned back by the phase increment caused by the frequency offset
             //std::cout << "TRA foff(*250e3):" << d_f_off*250e3 << ", phioff:" << d_phi_off << std::endl;
-            //correct_offsets(in, rem_f_off, rem_phi_off);
             correct_offsets(in, d_f_off, d_phi_off);
             d_phi_off = fmod(d_phi_off + 2*M_PI*d_f_off*d_L, 2*M_PI);
 
@@ -250,7 +245,7 @@ namespace gr {
         }
 
         // inform the scheduler about what has been going on...
-        // std::cout << "consumed:" << samples_consumed << ", returned:" << samples_returned << std::endl;
+        //std::cout << "consumed:" << samples_consumed << ", returned:" << samples_returned << std::endl;
         consume_each(samples_consumed);
         if(noutput_items < samples_returned)
           throw std::runtime_error(std::string("Output buffer too small"));
