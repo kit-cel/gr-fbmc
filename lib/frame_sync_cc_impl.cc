c52991e20a2c5c72a2d83fe0c4c60a9c6d793352
Felix Wunsch <uncnr@student.kit.edu>
2014-08-23 10:11:25 +0200

    implemented the frame sync algorithm. still needs debugging and maybe an averaging of the frequency offsets as well as an area-search during the sync validation step to account for drifting clocks or a sync that has been slightly off

--- lib/frame_sync_cc_impl.cc
+++ lib/frame_sync_cc_impl.cc
@@ -44,23 +44,22 @@ namespace gr {
               gr::io_signature::make(1, 1, sizeof(gr_complex)),
               gr::io_signature::make(1, 1, sizeof(gr_complex))),
                 d_L(L),
-                d_frame_len(frame_len),
+                d_frame_len(frame_len*L),
+                d_fixed_lag_lookahead(1.5*L),
                 d_preamble_sym(preamble_sym),
                 d_step_size(step_size),
                 d_threshold(threshold),
-                d_sync_valid(false),
-                d_tracking(false),
+                d_state(FRAME_SYNC_ACQUISITION),
                 d_num_consec_frames(0),
-                d_sym_ctr(0),
+                d_sample_ctr(0),
+                d_pretracking_window(2*L),
+                d_pretracking_ctr(0),
                 d_f_off(0),
                 d_phi_off(0)
     {
       dbg_fp = fopen("fs_lc.bin", "wb");
       dbg_fp2 = fopen("fs_in.bin", "wb");
       dbg_fp3 = fopen("fs_rc.bin", "wb");
-
-      if(d_step_size != 1)
-        throw std::runtime_error(std::string("Step size must be 1 at the moment because there is no sensible use implemented yet"));
 
       if(d_threshold <= 0 || d_threshold >= 1)
         throw std::runtime_error(std::string("Threshold must be between (0,1)")); 
@@ -74,7 +73,10 @@ namespace gr {
       volk_32fc_x2_conjugate_dot_prod_32fc(&d_preamble_energy, &d_preamble_sym[0], &d_preamble_sym[0], d_preamble_sym.size());
       d_preamble_energy = abs(d_preamble_energy);
 
-      set_min_noutput_items(d_L); // that's what is returned with each call to work
+      d_pretracking_buf.clear();
+      d_pretracking_buf.resize(d_preamble_sym.size(), 0);
+
+      set_min_noutput_items(d_pretracking_buf.size()); // that's what can be returned with each call to work
     }
 
     /*
@@ -91,43 +93,25 @@ namespace gr {
     frame_sync_cc_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
     {
         // this is a few samples more than we actually need
-        ninput_items_required[0] = std::max(2*d_L + d_step_size, int(d_preamble_sym.size()) + d_step_size);
+        ninput_items_required[0] = std::max(2*d_L + d_fixed_lag_lookahead + d_step_size, int(d_preamble_sym.size()) + d_step_size);
     }
 
     float 
-    frame_sync_cc_impl::estimate_f_off(gr_complex corr_val)
+    frame_sync_cc_impl::estimate_cfo(gr_complex corr_val)
     { 
       //std::cout << "corr:" << corr_val << ", f_off:" << 1.0/(2*M_PI*d_L)*arg(-corr_val) << std::endl;
-      return -1.0/(2*M_PI*d_L)*arg(-corr_val); 
+      return -1.0/(2*M_PI*d_L)*arg(corr_val); 
     }
 
-    float 
-    frame_sync_cc_impl::estimate_phi_off(gr_complex* rx_pil)
-    { 
-      //std::cout << "phi_off:" << arg(*rx_pil/d_ref_pil) << std::endl;
-      return arg(*rx_pil); 
-    } 
-
     gr_complex
-    frame_sync_cc_impl::fixed_lag_corr(gr_complex *x1, gr_complex *x2, gr_complex *a1)
+    frame_sync_cc_impl::fixed_lag_corr(gr_complex *x)
     {
-      // NOTE: This calculates the dot product of two vectors and divides it by the average power such that the result lies in [0,1]
-
       // results for auto- and crosscorrelation
       gr_complex xcorr = 0;
       gr_complex acorr = 0;
 
-      // the dot prods
-      // std::cout << "compare:";
-      // for(int i=0; i<d_L; i++)
-      //   std::cout << x1[i] << " ";
-      // std::cout << " with ";
-      // for(int i=0; i<d_L; i++)
-      //   std::cout << x2[i] << " ";
-      // std::cout << std::endl;
-
-      volk_32fc_x2_conjugate_dot_prod_32fc(&xcorr, x1, x2, d_L);
-      volk_32fc_x2_conjugate_dot_prod_32fc(&acorr, a1, a1, d_L*2);
+      volk_32fc_x2_conjugate_dot_prod_32fc(&xcorr, x, x+d_L, d_L);
+      volk_32fc_x2_conjugate_dot_prod_32fc(&acorr, x, x, d_L*2);
 
       // acorr is calculated over two symbols, so scale accordingly
       return gr_complex(2,0)*xcorr/acorr;
@@ -143,11 +127,19 @@ namespace gr {
       return corr/sqrt(d_preamble_energy*sig_energy);
     }
 
-    void
-    frame_sync_cc_impl::correct_offsets(gr_complex* buf, float f_off, float phi_prev)
+    std::string
+    frame_sync_cc_impl::print_state()
     {
-      for(int i=0; i<d_L; i++)
-        buf[i] *= exp(gr_complex(0,1)*gr_complex(-2*M_PI*f_off*i - phi_prev, 0));
+      if(d_state == FRAME_SYNC_ACQUISITION)
+        return "Acquisition";
+      else if(d_state == FRAME_SYNC_PRETRACKING)
+        return "Pretracking";
+      else if(d_state == FRAME_SYNC_TRACKING)
+        return "Tracking";
+      else if(d_state == FRAME_SYNC_VALIDATION)
+        return "Validation";
+      else
+        return "invalid state";
     }
 
     int
@@ -156,135 +148,172 @@ namespace gr {
                        gr_vector_const_void_star &input_items,
                        gr_vector_void_star &output_items)
     {
-        if( ninput_items[0] < 2*d_L)
-          throw std::runtime_error(std::string("Not enough input items"));
+      if( ninput_items[0] < 2*d_L)
+        throw std::runtime_error(std::string("Not enough input items"));
 
-        gr_complex *in = (gr_complex *) input_items[0];
-        gr_complex *out = (gr_complex *) output_items[0];
+      gr_complex *in = (gr_complex *) input_items[0];
+      gr_complex *out = (gr_complex *) output_items[0];
 
-        int samples_consumed = 0;
-        int samples_returned = 0;
+      int samples_consumed = 0;
+      int samples_returned = 0;
 
-        /* Acquisition algorithm:
-        * Step 1: Perform correlation of two subsequent symbols about 2 symbols in advance. 
-        *         The peak itself is too broad for a SOF detection but the phase around the maximum is very stable and can therefore be used for CFO correction
-        *         This is needed as first step because the frequency offset degrades the correlation to the reference symbol severely.
-        * Step 2: Perform correlation with reference symbol on the frequency corrected input signal to obtain the exact location of the peak and therefore the SOF
-        *
-        * Tracking algorithm:
-        * Assume the synchronization to be valid for a whole frame. 
-        * At the (expected) beginning of each frame perform a fixed lag correlation to update the frequency and phase offset.
-        * Then correlate with the reference symbol and confirm the validity of the expected SOF
-        */
+      std::cout << "ctw, state: " << print_state() << std::endl;
 
+      /* Acquisition algorithm:
+      * Step 1: Perform correlation of two subsequent symbols about 2 symbols in advance. This is very robust against frequency offsets.
+      *         The peak itself is too broad for a SOF detection but the phase around the maximum is very stable and can therefore be used for CFO correction
+      *         This is needed as first step because the frequency offset degrades the correlation to the reference symbol severely.
+      * Step 2: Perform correlation with reference symbol on the frequency corrected input signal to obtain the exact location of the peak and therefore the SOF
+      *         If this pre-tracking does not lead to a detection within the next L samples, go back to step 1
+      *
+      * Tracking algorithm:
+      * Assume the synchronization to be valid for a whole frame. 
+      * At the (expected) beginning of each frame perform a fixed lag correlation to update the frequency offset.
+      * Then correlate with the reference symbol for the phase offset and confirm the validity of the expected SOF
+      */
 
-        // there are 4 cases to distinguish:
-        // 1. no frame found, return no samples
-        // 2. frame was already found and sync is assumed to be valid, only return samples
-        // 3. frame found, sync validity expired, perform correlation in the next run
-        // 4. newly found frame, start returning samples
-
-        if(!d_tracking) // acquisition
+      if(d_state == FRAME_SYNC_ACQUISITION)
+      {
+        gr_complex res = fixed_lag_corr(in+d_fixed_lag_lookahead);
+        if(abs(res) > d_threshold)
         {
-          // perform a fixed lag correlation of the next two consecutive symbols
-          gr_complex lagged_corr_res = fixed_lag_corr(in, in+d_L, in);
-          gr_complex ref_corr_res = ref_corr(in);
-          fwrite(&lagged_corr_res, sizeof(gr_complex), 1, dbg_fp);
-          fwrite(&ref_corr_res, sizeof(gr_complex), 1, dbg_fp3);
-          fwrite(in, sizeof(gr_complex), 1, dbg_fp2);
-          samples_consumed = 1;
-
-        }
-        // frame start has already been detected
-        //if(d_sync_valid)
-        //{
-        //   // std::cout << "SYNC VALID" << std::endl;
-        //   correct_offsets(in, d_f_off, d_phi_off);
-        //   d_phi_off = fmod(d_phi_off + 2*M_PI*d_f_off*d_L, 2*M_PI);
-        //   memcpy(out, in, d_L*sizeof(gr_complex));
-        //   d_sym_ctr++;
-        //   samples_consumed = d_L;
-        //   samples_returned = d_L;
-
-        //   // in the next call to work, the preamble is searched and should be found immediately
-        //   if(d_sym_ctr >= d_frame_len)
-        //   {
-        //     d_sync_valid = false;
-        //     d_sym_ctr=0;
-        //   }
-        // }
-        // else if(d_tracking) // tracking
-        // {
-        //   // std::cout << "TRACKING" << std::endl;
-
-        //   gr_complex corr = corr_coef(in, &d_preamble_sym[0], in+d_L*d_overlap);
-        //   // std::cout << "TRA i:" << i << " corr:" << corr << std::endl;
+          std::cout << "ACQ->PRETRACK after successful fixed lag correlation" << std::endl;
+          d_state = FRAME_SYNC_PRETRACKING;
+          d_pretracking_ctr = 0;
           
-        //   // std::cout << "#frame:" << d_num_consec_frames << std::endl;
-        //   d_num_consec_frames++;
-        //   samples_consumed = d_L;
-        //   if(abs(corr) > d_threshold) // SOF found
-        //   {
-        //     // estimate and correct frequency and phase offsets
-        //     //d_f_off = estimate_f_off(corr);
-        //     //d_phi_off = fmod(estimate_phi_off(in+d_L*d_overlap) - 2*M_PI*d_f_off*d_L*d_overlap, 2*M_PI); // the estimated phase offset has to be turned back by the phase increment caused by the frequency offset
-        //     //std::cout << "TRA foff(*250e3):" << d_f_off*250e3 << ", phioff:" << d_phi_off << std::endl;
-        //     //correct_offsets(in, d_f_off, d_phi_off);
-        //     //d_phi_off = fmod(d_phi_off + 2*M_PI*d_f_off*d_L, 2*M_PI);
+          d_f_off = estimate_cfo(res);              
+          for(int i=0; i < d_preamble_sym.size(); i++)
+            in[i] *= exp(gr_complex(0,-2*M_PI*d_f_off*i));
+          d_phi_off = fmod(-2*M_PI*d_f_off*d_preamble_sym.size(), 2*M_PI);        
+          d_pretracking_buf.assign(in, in+d_preamble_sym.size());
+          
+          samples_consumed = d_preamble_sym.size();
+          samples_returned = 0;
+        }
+        else
+        {
+          samples_consumed = d_step_size;
+          samples_returned = 0;
+        }
+      }
+      else if(d_state == FRAME_SYNC_PRETRACKING)
+      {
+        if(!d_pretracking_buf.is_linearized())
+          d_pretracking_buf.linearize();
+        gr_complex res = ref_corr(&d_pretracking_buf[0]);
+        if(abs(res) > d_threshold)
+        {
+          std::cout << "PRETRACK->TRACK after successful reference correlation" << std::endl;
+          d_state = FRAME_SYNC_TRACKING;
 
-        //     // copy the first symbol
-        //     //memcpy(out, in, d_L*sizeof(gr_complex));
-        //     //d_sym_ctr = 1;
-        //     //d_sync_valid = true;            
-        //     //samples_returned = d_L;
-        //   }
-        //   else // sync lost
-        //   {
-        //     // std::cout << "frame_sync_cc: Synchronization lost after " << d_num_consec_frames << " consecutive frames, back to acquisition mode" << std::endl;
-        //     // d_tracking = false;
-        //     // samples_returned = 0;
-        //     // d_num_consec_frames = 0;
-        //   }   
-        // }
-        // else // acquisition, proceed one sample at a time
-        // {
-          // std::cout << "ACQUISITION" << std::endl;
-          // gr_complex corr = corr_coef(in+d_L*d_overlap, in+d_L*(d_overlap+1), in+d_L*d_overlap);    
-          // //std::cout << "ACQ corr:" << corr << std::endl;      
-          // if(abs(corr) > d_threshold)
-          // {
-          //   // estimate and correct offsets
-          //   d_f_off = estimate_f_off(corr);
-          //   d_phi_off = fmod(estimate_phi_off(in+d_L*d_overlap) - 2*M_PI*d_f_off*d_L*d_overlap, 2*M_PI);
-          //   std::cout << "ACQ foff(*250e3):" << d_f_off*250e3 << ", phioff:" << d_phi_off << std::endl;
-          //   correct_offsets(in, d_f_off, d_phi_off);
-          //   d_phi_off = fmod(d_phi_off+2*M_PI*d_f_off*d_L, 2*M_PI);
-          //   //std::cout << "ACQ updated phioff:" << d_phi_off << std::endl;
+          for(int i=0; i < d_pretracking_buf.size(); i++)
+            d_pretracking_buf[i] *= exp(gr_complex(0,-arg(res)));
+          d_phi_off += -arg(res);
+          memcpy(out, &d_pretracking_buf[0], d_pretracking_buf.size()*sizeof(gr_complex));
+          
+          d_sample_ctr = d_pretracking_buf.size();
 
-          //   // copy the first symbol
-          //   memcpy(out, in, d_L*sizeof(gr_complex));
-          //   d_sync_valid = true;
-          //   samples_returned = d_L;
-          //   d_sym_ctr = 1;
-          //   samples_consumed = d_L;
-          //   if(!d_tracking)
-          //     std::cout << "frame_sync_cc: SOF detected, enter tracking mode" << std::endl;
-          //   d_tracking = true;
-          // }
-          // else
-          // {
-          //   samples_consumed = d_step_size; 
-          // }
-              
-        //}
+          samples_consumed = 0;
+          samples_returned = d_preamble_sym.size();
+        }
+        else
+        {
+          in[0] *= exp(gr_complex(0,d_phi_off));
+          d_phi_off = fmod(d_phi_off-2*M_PI*d_f_off*1, 2*M_PI);
+          d_pretracking_buf.push_back(in[0]);
+          d_pretracking_ctr++;
+          if(d_pretracking_ctr > d_pretracking_window)
+          {
+             d_state = FRAME_SYNC_ACQUISITION;
+             std::cout << "PRETRACK->ACQ after " << d_pretracking_ctr << " reference correlation trials" << std::endl;           
+          }
 
-        // inform the scheduler about what has been going on...
-        //std::cout << "consumed:" << samples_consumed << ", returned:" << samples_returned << std::endl;
-        consume_each(samples_consumed);
-        if(noutput_items < samples_returned)
-          throw std::runtime_error(std::string("Output buffer too small"));
+          samples_consumed = 1;
+          samples_returned = 0;
+        }
+      }
+      else if(d_state == FRAME_SYNC_TRACKING)
+      {
+        int samples_until_eof = d_frame_len - d_sample_ctr;
+        int samples_to_return = std::min(samples_until_eof, ninput_items[0]);
+        samples_to_return = std::min(samples_to_return, noutput_items);
 
-        return samples_returned;            
+        for(int i=0; i < samples_to_return; i++)
+          in[i] *= exp(gr_complex(0,-2*M_PI*d_f_off*i + d_phi_off));
+        d_phi_off = fmod(d_phi_off - 2*M_PI*d_f_off*samples_to_return, 2*M_PI);
+
+        d_sample_ctr += samples_to_return;
+        if(d_sample_ctr == d_frame_len)
+        {
+          d_state = FRAME_SYNC_VALIDATION;
+          std::cout << "TRACK->VAL after a complete frame" << std::endl;          
+        }
+
+        samples_consumed = samples_to_return;
+        samples_returned = samples_to_return;
+      }
+      else if(d_state == FRAME_SYNC_VALIDATION)
+      {
+        d_sample_ctr = 0;
+        gr_complex res = fixed_lag_corr(in+d_fixed_lag_lookahead);
+        if( abs(res) > d_threshold )
+        {
+          d_f_off = estimate_cfo(res);
+
+          d_pretracking_buf.assign(in, in+d_preamble_sym.size());
+          for(int i=0; i < d_pretracking_buf.size(); i++)
+            d_pretracking_buf[i] *= exp(gr_complex(0,-2*M_PI*d_f_off*i));
+          d_phi_off = -2*M_PI*d_f_off*d_pretracking_buf.size();
+
+          if(!d_pretracking_buf.is_linearized())
+            d_pretracking_buf.linearize();
+          
+          res = ref_corr(&d_pretracking_buf[0]);
+          if(abs(res) > d_threshold)
+          {
+            std::cout << "VAL->TRACK after successfull fixed lag and reference correlation" << std::endl;
+            d_state = FRAME_SYNC_TRACKING;
+
+            for(int i=0; i < d_pretracking_buf.size(); i++)
+              d_pretracking_buf[i] *= exp(gr_complex(0,-arg(res)));
+            d_phi_off += -arg(res);
+
+            memcpy(out, &d_pretracking_buf[0], sizeof(gr_complex)*d_pretracking_buf.size());
+            d_sample_ctr = d_pretracking_buf.size();
+
+            samples_consumed = d_preamble_sym.size();
+            samples_returned = d_preamble_sym.size();
+          }
+          else
+          {
+            std::cout << "VAL->ACQ after unsuccessful reference correlation" << std::endl;
+            d_state = FRAME_SYNC_ACQUISITION;
+
+            samples_consumed = 1;
+            samples_returned = 0;
+          }
+        }
+        else
+        {
+          std::cout << "VAL->ACQ after unsuccessful fixed lag correlation" << std::endl;
+          d_state = FRAME_SYNC_ACQUISITION;
+
+          samples_consumed = 1;
+          samples_returned = 0;
+        }
+
+      }
+      else
+        throw std::runtime_error("frame synchronization: invalid state");
+
+
+      // inform the scheduler about what has been going on...
+      //std::cout << "consumed:" << samples_consumed << ", returned:" << samples_returned << std::endl;
+      consume_each(samples_consumed);
+      if(noutput_items < samples_returned)
+        throw std::runtime_error(std::string("Output buffer too small"));
+
+      return samples_returned;            
     }
 
   } /* namespace fbmc */
--- lib/frame_sync_cc_impl.h
+++ lib/frame_sync_cc_impl.h
@@ -28,28 +28,34 @@
 namespace gr {
   namespace fbmc {
 
+    unsigned const FRAME_SYNC_ACQUISITION = 0;
+    unsigned const FRAME_SYNC_PRETRACKING = 1;
+    unsigned const FRAME_SYNC_TRACKING = 2;
+    unsigned const FRAME_SYNC_VALIDATION = 3;
+
     class frame_sync_cc_impl : public frame_sync_cc {
      private:
       int d_L; // number of subcarriers
-      int d_frame_len; // number of symbols per frame (including preamble)
+      int d_frame_len; // number of samples per frame (including preamble)
+      int d_fixed_lag_lookahead; // lookahead for fixed lag correlation
       std::vector<gr_complex> d_preamble_sym; // reference preamble symbol
       gr_complex d_preamble_energy; // energy of the preamble signal
       int d_step_size; // number of samples to proceed with every step
       float d_threshold; // threshold for the correlation
-
-      bool d_tracking; // flag indicating whether the sync is in tracking (true) or acquisition mode (false)
-      bool d_sync_valid; // flag indicating whether a the current sync is still vlid
+      unsigned int d_state; // synchronization state
       int d_num_consec_frames; // number of consecutive frames detected
-      int d_sym_ctr; // number of symbols of the current frame that already have been written
+      int d_sample_ctr; // number of samples of the current frame that already have been written
+      int d_pretracking_window; // time window for which the pretracking is performed before going back to acquisition
+      int d_pretracking_ctr; // counter for determining the position in the pretracking window
+      boost::circular_buffer<gr_complex> d_pretracking_buf; // buffer for incoming samplesl during pretracking
 
       float d_f_off; // estimated frequency offset
       float d_phi_off; // estimated phase offset, updated in every step to avoid phase discontinuities
 
-      gr_complex fixed_lag_corr(gr_complex *x1, gr_complex *x2, gr_complex *a1); // fixed lag correlation
+      gr_complex fixed_lag_corr(gr_complex *x); // fixed lag correlation
       gr_complex ref_corr(gr_complex *x); // reference correlation
-      float estimate_f_off(gr_complex corr_val); // estimate frequency offset
-      float estimate_phi_off(gr_complex* rx_pil); // estimate phase offset
-      void correct_offsets(gr_complex* buf, float f_off, float phi_prev); // correct phase and frequency offset, start with defined phase offset to avoid discontiuities between symbols
+      float estimate_cfo(gr_complex corr_val); // estimate carrier frequency offset
+      std::string print_state();
 
       FILE* dbg_fp;
       FILE* dbg_fp2;
