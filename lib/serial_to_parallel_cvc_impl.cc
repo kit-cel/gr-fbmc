632a5f335bc9ac434aaeef376d2dd788a59568cd
Johannes Demel <ufcsy@student.kit.edu>
2014-10-13 17:29:23 +0200

    cosmetics. you need them.

--- lib/serial_to_parallel_cvc_impl.cc
+++ lib/serial_to_parallel_cvc_impl.cc
@@ -26,60 +26,58 @@
 #include "serial_to_parallel_cvc_impl.h"
 
 namespace gr {
-  namespace fbmc {
+namespace fbmc {
 
-    serial_to_parallel_cvc::sptr
-    serial_to_parallel_cvc::make(int len_in, int vlen_out, std::vector<int> channel_map)
-    {
-      return gnuradio::get_initial_sptr
-        (new serial_to_parallel_cvc_impl(len_in, vlen_out, channel_map));
-    }
+serial_to_parallel_cvc::sptr serial_to_parallel_cvc::make(int len_in,
+        int vlen_out, std::vector<int> channel_map) {
+    return gnuradio::get_initial_sptr(
+            new serial_to_parallel_cvc_impl(len_in, vlen_out, channel_map));
+}
 
-    /*
-     * The private constructor
-     */
-    serial_to_parallel_cvc_impl::serial_to_parallel_cvc_impl(int len_in, int vlen_out, std::vector<int> channel_map)
-      : gr::sync_decimator("serial_to_parallel_cvc",
-              gr::io_signature::make(1, 1, sizeof(gr_complex)),
-              gr::io_signature::make(1, 1, sizeof(gr_complex)*vlen_out), len_in),
-              d_len_in(len_in),
-              d_vlen_out(vlen_out),
-              d_channel_map(channel_map)
-    {
-    	if(d_len_in > d_vlen_out)
-        throw std::runtime_error(std::string("Output vector length must at least be equal to the input length"));
-      else if(d_channel_map.size() != d_vlen_out)
-        throw std::runtime_error(std::string("Channel map does not match the output vector length"));
-    }
+/*
+ * The private constructor
+ */
+serial_to_parallel_cvc_impl::serial_to_parallel_cvc_impl(int len_in,
+        int vlen_out, std::vector<int> channel_map) :
+        gr::sync_decimator("serial_to_parallel_cvc",
+                gr::io_signature::make(1, 1, sizeof(gr_complex)),
+                gr::io_signature::make(1, 1, sizeof(gr_complex) * vlen_out),
+                len_in), d_len_in(len_in), d_vlen_out(vlen_out), d_channel_map(
+                channel_map) {
+    if (d_len_in > d_vlen_out)
+        throw std::runtime_error(
+                std::string(
+                        "Output vector length must at least be equal to the input length"));
+    else if (d_channel_map.size() != d_vlen_out)
+        throw std::runtime_error(
+                std::string(
+                        "Channel map does not match the output vector length"));
+}
 
-    /*
-     * Our virtual destructor.
-     */
-    serial_to_parallel_cvc_impl::~serial_to_parallel_cvc_impl()
-    {
-    }
+/*
+ * Our virtual destructor.
+ */
+serial_to_parallel_cvc_impl::~serial_to_parallel_cvc_impl() {
+}
 
-    int
-    serial_to_parallel_cvc_impl::work(int noutput_items,
-			  gr_vector_const_void_star &input_items,
-			  gr_vector_void_star &output_items)
-    {
-        gr_complex *in = (gr_complex *) input_items[0];
-        gr_complex *out = (gr_complex *) output_items[0];
+int serial_to_parallel_cvc_impl::work(int noutput_items,
+        gr_vector_const_void_star &input_items,
+        gr_vector_void_star &output_items) {
+    gr_complex *in = (gr_complex *) input_items[0];
+    gr_complex *out = (gr_complex *) output_items[0];
 
-        // convert sample stream to vector, use channel map
-        for(int i=0; i < d_vlen_out; i++)
-        {
-          if(d_channel_map[i] == 0)
-            *out++ = gr_complex(0,0);
-          else
+    // convert sample stream to vector, use channel map
+    for (int i = 0; i < d_vlen_out; i++) {
+        if (d_channel_map[i] == 0)
+            *out++ = gr_complex(0, 0);
+        else
             *out++ = *in++;
-        }
-
-        // Tell runtime system how many output items we produced.
-        return 1;
     }
 
-  } /* namespace fbmc */
+    // Tell runtime system how many output items we produced.
+    return 1;
+}
+
+} /* namespace fbmc */
 } /* namespace gr */
 
--- python/qa_frame_generator_vcvc.py
+++ python/qa_frame_generator_vcvc.py
@@ -23,91 +23,95 @@ from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 import fbmc_swig as fbmc
 
-class qa_frame_generator_vcvc (gr_unittest.TestCase):
 
-	def setUp (self):
-		self.tb = gr.top_block ()
+class qa_frame_generator_vcvc(gr_unittest.TestCase):
+    def setUp(self):
+        self.tb = gr.top_block()
 
-	def tearDown (self):
-		self.tb = None
+    def tearDown(self):
+        self.tb = None
 
-	def test_001_t (self): # forward
-		print "test 1 - forward"
-		L=8
-		self.src = blocks.vector_source_c(range(1,L+1), vlen=L, repeat=False)
-		self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload = 1, inverse=0, num_overlap = 4, num_sync = 3) # the frame len includes the length of the overlap
-		self.snk = blocks.vector_sink_c(vlen=L)
-		self.tb.connect(self.src, self.frame_gen, self.snk)
-		self.tb.run ()
-		# check data
-		ref = (0,0,0,0,0,0,0,0, 
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   1,2,3,4,5,6,7,8,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0 )
-		data = self.snk.data()
-		self.assertComplexTuplesAlmostEqual(ref, data)
+    def test_001_t(self):  # forward
+        print "test 1 - forward"
+        L = 8
+        self.src = blocks.vector_source_c(range(1, L + 1), vlen=L, repeat=False)
+        self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload=1, inverse=0, num_overlap=4,
+                                                   num_sync=3)  # the frame len includes the length of the overlap
+        self.snk = blocks.vector_sink_c(vlen=L)
+        self.tb.connect(self.src, self.frame_gen, self.snk)
+        self.tb.run()
+        # check data
+        ref = (0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               1, 2, 3, 4, 5, 6, 7, 8,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0,
+               0, 0, 0, 0, 0, 0, 0, 0 )
+        data = self.snk.data()
+        self.assertComplexTuplesAlmostEqual(ref, data)
 
-	def test_002_t (self): # reverse
-		print "test 2 - reverse"
-		L = 8
-		input_data = (0,0,0,0,0,0,0,0, 
-			   1,1,-1,-1,1,1,-1,-1,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   0,0,0,0,0,0,0,0,
-			   1,2,3,4,5,6,7,8)
-		self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
-		self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload = 1, inverse=1, num_overlap = 4, num_sync = 3)
-		self.snk = blocks.vector_sink_c(vlen=L)
-		self.tb.connect(self.src, self.frame_gen, self.snk)
-		self.tb.run()
-		# check data
-		data = self.snk.data()
-		ref = (1,2,3,4,5,6,7,8)
-		self.assertComplexTuplesAlmostEqual(ref, data)
+    def test_002_t(self):  # reverse
+        print "test 2 - reverse"
+        L = 8
+        input_data = (0, 0, 0, 0, 0, 0, 0, 0,
+                      1, 1, -1, -1, 1, 1, -1, -1,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0,
+                      1, 2, 3, 4, 5, 6, 7, 8)
+        self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
+        self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload=1, inverse=1, num_overlap=4, num_sync=3)
+        self.snk = blocks.vector_sink_c(vlen=L)
+        self.tb.connect(self.src, self.frame_gen, self.snk)
+        self.tb.run()
+        # check data
+        data = self.snk.data()
+        ref = (1, 2, 3, 4, 5, 6, 7, 8)
+        self.assertComplexTuplesAlmostEqual(ref, data)
 
-	def test_003_t (self): # forward
-		print "test 3 - forward - many frames"
-		L=8
-		input_data = range(1,161)
-		self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
-		self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload = 1, inverse=0, num_overlap = 4, num_sync = 3) # the frame len includes the length of the overlap
-		self.snk = blocks.vector_sink_c(vlen=L)
-		self.tb.connect(self.src, self.frame_gen, self.snk)
-		self.tb.run ()
-		# check data
-		data = self.snk.data()
-		self.assertEqual(len(data), len(input_data)*12)
+    def test_003_t(self):  # forward
+        print "test 3 - forward - many frames"
+        L = 8
+        input_data = range(1, 161)
+        self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
+        self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload=1, inverse=0, num_overlap=4,
+                                                   num_sync=3)  # the frame len includes the length of the overlap
+        self.snk = blocks.vector_sink_c(vlen=L)
+        self.tb.connect(self.src, self.frame_gen, self.snk)
+        self.tb.run()
+        # check data
+        data = self.snk.data()
+        self.assertEqual(len(data), len(input_data) * 12)
 
-	def test_004_t (self): # reverse
-		print "test 4 - reverse - many frames"
-		L = 8
-		K = 5
-		num_frames = 10
-		input_data = range(1,(K+11)*L*num_frames+1)
-		self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
-		self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload = K, inverse=1, num_overlap = 4, num_sync = 3)
-		self.snk = blocks.vector_sink_c(vlen=L)
-		self.tb.connect(self.src, self.frame_gen, self.snk)
-		self.tb.run()
-		# check data
-		data = self.snk.data()
-		self.assertEqual(len(data), L*K*num_frames)
-"""	
+    def test_004_t(self):  # reverse
+        print "test 4 - reverse - many frames"
+        L = 8
+        K = 5
+        num_frames = 10
+        input_data = range(1, (K + 11) * L * num_frames + 1)
+        self.src = blocks.vector_source_c(input_data, vlen=L, repeat=False)
+        self.frame_gen = fbmc.frame_generator_vcvc(sym_len=L, num_payload=K, inverse=1, num_overlap=4, num_sync=3)
+        self.snk = blocks.vector_sink_c(vlen=L)
+        self.tb.connect(self.src, self.frame_gen, self.snk)
+        self.tb.run()
+        # check data
+        data = self.snk.data()
+        self.assertEqual(len(data), L * K * num_frames)
+
+
+"""
 	def test_005_t (self): # forward
 		print "test 5 - forward - many long frames"
 		L=16
@@ -141,4 +145,4 @@ class qa_frame_generator_vcvc (gr_unittest.TestCase):
 		self.assertEqual(len(data), num_frames*L*K)			
 """
 if __name__ == '__main__':
-	gr_unittest.run(qa_frame_generator_vcvc)
+    gr_unittest.run(qa_frame_generator_vcvc)
